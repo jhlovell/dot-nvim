9218ffd24a18b9486d0972d575746c94eb93fd73
Jacob Lovell <jlovell@retiamedical.com>
2024-03-29 17:49:09 -0400

    add qt to treesitter

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -1,12 +1,10 @@
 return {
   'catppuccin/nvim',
-  lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    -- Load the colorscheme here
+  priority = 1000, -- Make sure to load this before all the other start plugins.
+  init = function()
     vim.cmd.colorscheme 'catppuccin-mocha'
 
-    -- You can configure highlights by doing something like
+    -- You can configure highlights by doing something like:
     vim.cmd.hi 'Comment gui=none'
   end,
 }
--- lua/custom/plugins/tokyonight.lua
+++ lua/custom/plugins/tokyonight.lua
@@ -1,8 +1,4 @@
-return { -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+return {
   'folke/tokyonight.nvim',
-  config = function() end,
+  init = function() end,
 }
