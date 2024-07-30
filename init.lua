21bd711f391532f2bfaac1aba79be4b54f9970dc
Jacob Lovell <jlovell@retiamedical.com>
2024-07-30 10:35:56 -0400

    add pyright and fix keymaps

--- init.lua
+++ init.lua
@@ -588,7 +588,7 @@ require('lazy').setup({
         },
         gopls = {},
         jdtls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
