6ce07b38bb5d180dd932a2dc7378cc2fa34b81a0
Jacob Lovell <jlovell@retiamedical.com>
2024-08-29 11:36:59 -0400

    add clang-format

--- init.lua
+++ init.lua
@@ -669,6 +669,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'clang-format',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -721,6 +722,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
